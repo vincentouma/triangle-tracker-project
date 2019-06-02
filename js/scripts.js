c6e5aa1fe6434b41a3db1a0f99f66bc9a7859d5e
vincentouma <vinceoumah@gmail.com>
2019-06-02 22:28:14 +0300

    Add all changesin css file

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,16 @@ function triangle(){
     var a = document.getElementById("a").value;
     var b= document.getElementById("b").value
     var c = document.getElementById("c").value;
+
+    if(  a == "" || b=="" || c==""){
+        alert("Enter a number");
+
+        return
+    }
+    else if (a<=0 || b<=0 || c<=0) {
+        alert ("Not a triangle");
+        return
+    }
     
     if (a===b && b===c && c===a) {
         alert("equilateral");
@@ -18,6 +28,10 @@ function triangle(){
     }
     else if (a+b<=c || b+C <=a || c+a<=b){
         alertt("triangle cannot be formed");
+        
+    }
+    else if (a=0, b=0, c=0) {
+        alert ("not a triangle");
     }
     else
     alert("please ckeck your values")
